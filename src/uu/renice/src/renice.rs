a010a9a44a1cb70bfa075a1b04cbb30c0812886a
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2024-04-08 14:02:43 +0200

    renice: separate consts and imports by empty line

--- src/uu/renice/src/renice.rs
+++ src/uu/renice/src/renice.rs
@@ -3,15 +3,16 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
+use clap::{crate_version, Arg, Command};
 #[cfg(not(windows))]
 use libc::PRIO_PROCESS;
 use std::env;
 use std::io::Error;
 use std::process;
 use uucore::{error::UResult, format_usage, help_about, help_usage};
+
 const ABOUT: &str = help_about!("renice.md");
 const USAGE: &str = help_usage!("renice.md");
-use clap::{crate_version, Arg, Command};
 
 #[uucore::main]
 pub fn uumain(args: impl uucore::Args) -> UResult<()> {
